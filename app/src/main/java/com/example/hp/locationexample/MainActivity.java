2ad3847c1a12622be25cb2b5d683636ad8ad63e2
Hp <https://github.com/indrajeettri/myfristgithub.git>
2019-11-21 14:18:17 +0530

    Langitude and Latitude

---
+++ app/src/main/java/com/example/hp/locationexample/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.hp.locationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etPlace;
+    Button btSubmit;
+    TextView tvAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etPlace = findViewById(R.id.et_place);
+        btSubmit = findViewById(R.id.bt_submit);
+        tvAddress = findViewById(R.id.tv_address);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = etPlace.getText().toString();
+                GeoLocation geoLocation = new GeoLocation();
+                geoLocation.getAddress(address, getApplicationContext(), new GeoHandler());
+            }
+        });
+    }
+
+        public class GeoHandler extends Handler {
+            @Override
+            public void handleMessage(Message msg) {
+                String address;
+                switch (msg.what){
+                    case 1:
+                        Bundle bundle=msg.getData();
+                        address=bundle.getString("address");
+                        break;
+                    default:
+                        address=null;
+                }
+                tvAddress.setText(address);
+            }
+        }
+    }
+
